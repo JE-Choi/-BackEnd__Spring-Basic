eb4efd34556e5e06d9f18d24f7f99a4734018847
최지은 <lielieing@naver.com>
2021-10-17 19:54:58 +0900

    version 3.3 xml로 의존성 주입 테스트

---
+++ src/test/java/com/jechoi/core/xml/XmlAppContextTest.java
@@ -0,0 +1,16 @@
+package com.jechoi.core.xml;
+
+import com.jechoi.core.member.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContextTest {
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertEquals(true, memberService instanceof MemberService);
+    }
+}
