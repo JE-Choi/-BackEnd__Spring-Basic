d5ff4ba8eb9c03be1146f96331dc63cf6744fed1
최지은 <lielieing@naver.com>
2021-10-25 00:15:00 +0900

    싱글톤 패턴을 적용한 객체 사용

--- src/test/java/com/jechoi/core/singleton/SingletomTest.java
+++ src/test/java/com/jechoi/core/singleton/SingletomTest.java
@@ -5,6 +5,8 @@ import com.jechoi.core.member.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletomTest {
     @Test
@@ -20,10 +22,23 @@ public class SingletomTest {
         // 참조값이 다른것을 확인
         Assertions.assertEquals(false, memberService1 == memberService2);
     }
+
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
     void singletonServiceTest() {
         // 참조값이 같은 것을 확인
         Assertions.assertEquals(true, SingletonService.getInstance() == SingletonService.getInstance());
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = configApplicationContext.getBean("memberService", MemberService.class);
+        MemberService memberService2 = configApplicationContext.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은것을 확인
+        Assertions.assertEquals(true, memberService1 == memberService2);
     }
 }
