d45252a03a0b4b2d0417cdc6094eca504b21372e
최지은 <lielieing@naver.com>
2021-11-28 16:59:06 +0900

    [끝!!] 스코프와 프록시

--- src/main/java/com/jechoi/core/web/LogDemoController.java
+++ src/main/java/com/jechoi/core/web/LogDemoController.java
@@ -6,21 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final Provider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping(value = "log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        // 아래 로직은 인터셉터에서 공통관리하면 좋음.
-        final MyLogger myLogger = this.myLoggerProvider.get();
+        // 아래 로직은 인터셉터에서 공통관리하면 좋음
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("LogDemoController test");
